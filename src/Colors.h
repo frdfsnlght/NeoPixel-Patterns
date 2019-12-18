9c683605eef6d1bac8301ee25ef3d9c334325679
Thomas Bennedum <tab@bennedum.org>
2019-12-18 10:53:58 -0500

    Added SCALECOLOR

--- src/Colors.h
+++ src/Colors.h
@@ -11,6 +11,7 @@ typedef uint32_t        color_t;
 #define GREEN(color)    ((color>>8)&0xff)
 #define BLUE(color)     (color&0xff)
 #define DIMCOLOR(color) (COLOR(RED(color)>>1,GREEN(color)>>1,BLUE(color)>>1))
+#define SCALECOLOR(color,s) (COLOR((int)((float)RED(color)*s),(int)((float)GREEN(color)*s),(int)((float)BLUE(color)*s)))
 
 #define COLOR_OFF       (COLOR(0,0,0))
 #define COLOR_WHITE     (COLOR(255,255,255))
